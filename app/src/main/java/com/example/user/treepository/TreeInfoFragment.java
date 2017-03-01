d4a6c3519aa0f7e7843e98e87271c45fb77733e5
boulwabd <brycebware@gmail.com>
2017-03-01 15:09:53 -0500

    Share button functionality

--- app/src/main/java/com/example/user/treepository/TreeInfoFragment.java
+++ app/src/main/java/com/example/user/treepository/TreeInfoFragment.java
@@ -29,15 +29,27 @@ import java.util.Locale;
  * Created by brycebware on 11/24/16.
  */
 
-public class TreeInfoFragment extends Fragment {
+public class TreeInfoFragment extends Fragment implements View.OnClickListener {
     private TextView textViewTreeInfo;
+    private Button buttonShare;
     View view;
+    String treeName;
+    String address;
+    String age;
+    String description;
+    String height;
+    String lat;
+    String lifeSpan;
+    String longitude;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_treeinfo,container,false);
+        buttonShare = (Button) view.findViewById(R.id.buttonShare);
+        buttonShare.setOnClickListener(this);
         textViewTreeInfo = (TextView) view.findViewById(R.id.textViewTreeInfo);
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
         DatabaseReference treeRef = ref.child(MainActivity.currentTreeKey);
@@ -45,14 +57,14 @@ public class TreeInfoFragment extends Fragment {
         treeRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
-                String treeName = snapshot.child("type").getValue().toString();
-                String address = snapshot.child("address").getValue().toString();
-                String age = snapshot.child("age").getValue().toString();
-                String description = snapshot.child("description").getValue().toString();
-                String height = snapshot.child("height").getValue().toString();
-                String lat = snapshot.child("latitude").getValue().toString();
-                String lifeSpan = snapshot.child("lifeSpan").getValue().toString();
-                String longitude = snapshot.child("longitude").getValue().toString();
+                treeName = snapshot.child("type").getValue().toString();
+                address = snapshot.child("address").getValue().toString();
+                age = snapshot.child("age").getValue().toString();
+                description = snapshot.child("description").getValue().toString();
+                height = snapshot.child("height").getValue().toString();
+                lat = snapshot.child("latitude").getValue().toString();
+                lifeSpan = snapshot.child("lifeSpan").getValue().toString();
+                longitude = snapshot.child("longitude").getValue().toString();
                     //Adding it to a string
                     String string = "Type: " + treeName + "\nAddress: " + address;
                     string += "\nAge: " + age + "\nHeight: " + height;
@@ -70,6 +82,16 @@ public class TreeInfoFragment extends Fragment {
         });
 
         return view;
+
+    }
+    public void onClick(View v) {
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        String shareBody = "Check out this " + treeName + " tree at " + address + ".\nDownload the" +
+                " Treasured Trees App to find more trees near you!";
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Treasured Trees");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+        startActivity(Intent.createChooser(sharingIntent, "Share via"));
 
     }
 
